e9ded9483f3d3fd3a2f8f1a98308730252b6ca31
zhaoke.deng <dengzhaoke@outlook.com>
2019-06-11 13:43:53 +0800

    feat(首页): 新增类目

--- src/page/index/Home/Category/Category.jsx
+++ src/page/index/Home/Category/Category.jsx
@@ -1,21 +1,54 @@
 import React from 'react';
 import { Grid } from 'antd-mobile';
+import axios from 'axios';
 import './Category.scss';
 
 /**
  * @constructor <Category />
  * @description 外卖类别
  */
-const data = Array.from(new Array(8)).map((_val, i) => ({
-  icon: 'https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png',
-  text: `name${i}`,
-}));
 
 export default class Category extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.fetchData();
+    this.state = {
+      categoryList: [],
+    }
+  }
+
+  fetchData = () => {
+    axios({
+      method: 'get',
+      url: '/json/head.json'
+    }).then(res => {
+      const data = res.data.data.primary_filter;
+      if(Array.isArray(data) || data.length) {
+        const categoryList = data.map(e => {
+          return {
+            icon: e.url,
+            text: e.name,
+            code: e.code,
+          }
+        });
+        this.setState({categoryList});
+      }
+    }).catch(() => {
+      new Error('请求失败！')
+    })
+  };
+
   render() {
+    const { categoryList } = this.state;
     return (
-      <Grid data={data} activeStyle={false} />
+      <Grid
+        data={categoryList}
+        activeStyle={false}
+        hasLine={false}
+        isCarousel={true}
+        carouselMaxRow={2}
+      />
     )
   }
 }
---
+++ src/page/index/Home/ContentList/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * @constructor <Demo />
+ * @description Demo
+ */
+
+export default class Index extends React.Component {
+
+  render() {
+    return (
+      <h1>hello world</h1>
+    )
+  }
+}
